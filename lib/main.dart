fcce475607d1e04be5d85ba404f734549fcd656d
Abdullah Maqsood <abdullahmaqsood2002@gmail.com>
2025-02-28 15:44:25 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,118 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/api/call.dart';
-import 'package:my_first_app/api/product_model.dart';
-import 'package:my_first_app/widgets/card.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const CaseTrackerApp());
 }
 
-class MyApp extends StatelessWidget {
+class CaseTrackerApp extends StatelessWidget {
+  const CaseTrackerApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Products Grid',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: ProductGridScreen(),
+      theme: ThemeData.light(),
+      home: const CaseTrackerScreen(),
     );
   }
 }
 
-class ProductGridScreen extends StatelessWidget {
+class CaseTrackerScreen extends StatelessWidget {
+  const CaseTrackerScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Products')),
-      body: FutureBuilder<List<Products>>(
-        future: fetchProducts(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No products found!'));
-          }
-
-          List<Products> products = snapshot.data!;
-
-          return Padding(
-            padding: EdgeInsets.all(8.0),
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
-                childAspectRatio: 0.8,
-              ),
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                return ProductCard(product: products[index]);
-              },
-            ),
-          );
-        },
+      appBar: AppBar(
+        title: const Text(
+          "Case Tracker",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView.builder(
+          itemCount: DataPoint.values.length,
+          itemBuilder: (context, index) {
+            return _buildCaseStat(DataPoint.values[index]);
+          },
+        ),
       ),
     );
   }
+
+  Widget _buildCaseStat(DataPoint data) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.black54,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Align title to the left
+        children: [
+          Text(
+            data.name,
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: data.color),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    data.assetPath,
+                    height: 40,
+                    width: 40,
+                    color: data.color,
+                  ),
+                  const SizedBox(width: 15),
+                ],
+              ),
+              Text(
+                _getCaseValue(data),
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: data.color),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getCaseValue(DataPoint data) {
+    switch (data) {
+      case DataPoint.casesTotal:
+        return "9,231,249";
+      case DataPoint.casesActive:
+        return "123,214";
+      case DataPoint.deaths:
+        return "51,245";
+      case DataPoint.recovered:
+        return "7,452,340";
+    }
+  }
+}
+
+enum DataPoint {
+  casesTotal('Total Cases', 'assets/count.png', Color(0xFFFFF492)),
+  casesActive('Active Cases', 'assets/fever.png', Color(0xFFE99600)),
+  deaths('Deaths', 'assets/death.png', Color(0xFFE40000)),
+  recovered('Recovered', 'assets/patient.png', Color(0xFF70A901));
+
+  const DataPoint(this.name, this.assetPath, this.color);
+  final String name;
+  final String assetPath;
+  final Color color;
 }
